134b9ff691333ce58e2cb2dd4d1a2ce06d7288ea
Sungoh Kim <sungohki@c7r6s7.42seoul.kr>
2022-11-28 17:42:20 +0900

    strmapi 0

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: sungohki <sungohki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 09:55:25 by sungohki          #+#    #+#             */
-/*   Updated: 2022/11/23 16:07:12 by sungohki         ###   ########.fr       */
+/*   Updated: 2022/11/28 17:37:52 by sungohki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,15 @@
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	char			*result;
-	unsigned int	index;
-	unsigned int	len;
+	char	*result;
+	size_t	len;
 
-	len = 0;
-	while (s[len])
-		len++;
+	len = ft_strlen(s);
 	result = (char *)malloc(sizeof(char) * (len + 1));
 	if (result == 0)
 		return (0);
-	index = 0;
-	while (s[index])
-	{
-		result[index] = (*f)(index, s[index]);
-		index++;
-	}
-	result[index] = 0;
+	result[len] = 0;
+	while (len--)
+		result[len] = (*f)(len, s[len]);
 	return (result);
 }
