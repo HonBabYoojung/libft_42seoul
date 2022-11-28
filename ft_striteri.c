47f944c5d4f1b29ff724e2041eff11c451284c33
Sungoh Kim <sungohki@c7r6s7.42seoul.kr>
2022-11-28 17:52:44 +0900

    striteri 0

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: sungohki <sungohki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 10:53:55 by sungohki          #+#    #+#             */
-/*   Updated: 2022/11/23 16:07:16 by sungohki         ###   ########.fr       */
+/*   Updated: 2022/11/28 17:52:18 by sungohki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,9 @@
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
-	unsigned int		len;
-	unsigned int		index;
+	size_t	len;
 
-	len = 0;
-	while (s[len])
-		len++;
-	index = 0;
-	while (s[index])
-	{
-		(*f)(index, s);
-		index++;
-	}
+	len = ft_strlen(s);
+	while (len--)
+		(*f)(len, s);
 }
