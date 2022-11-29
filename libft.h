f1941ce12714171c743ce7099f13418b18dc0c08
Sungoh Kim <sungohki@c6r3s2.42seoul.kr>
2022-11-29 17:19:58 +0900

    add t_list

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: sungohki <sungohki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 18:15:46 by sungohki          #+#    #+#             */
-/*   Updated: 2022/11/28 17:00:40 by sungohki         ###   ########.fr       */
+/*   Updated: 2022/11/29 15:52:59 by sungohki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,10 @@ void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memset(void *b, int c, size_t len);
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
 
 #endif
