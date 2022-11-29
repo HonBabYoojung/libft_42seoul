77fdd996bc6816a2c784b95b0c27769338c321f5
Sungoh Kim <sungohki@c6r3s2.42seoul.kr>
2022-11-29 17:19:29 +0900

    lst start

---
+++ ft_lstsize.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sungohki <sungohki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/29 17:08:50 by sungohki          #+#    #+#             */
+/*   Updated: 2022/11/29 17:14:48 by sungohki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_lstsize(t_list *lst)
+{
+	t_list	*temp;
+	int		count;
+
+	temp = lst;
+	count = 0;
+	if (lst == 0)
+		return (0);
+	while (temp->next != 0)
+	{
+		temp = temp->next;
+		count++;
+	}
+	return (count);
+}
