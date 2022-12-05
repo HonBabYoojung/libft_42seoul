5885180e2c398716c268039b8026c7276e2d4049
Sungoh Kim <sungohki@c6r2s8.42seoul.kr>
2022-12-05 23:09:44 +0900

    putstr_fd add null guard

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: sungohki <sungohki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 12:10:16 by sungohki          #+#    #+#             */
-/*   Updated: 2022/12/05 20:42:02 by sungohki         ###   ########.fr       */
+/*   Updated: 2022/12/05 23:06:12 by sungohki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	ft_putstr_fd(char *s, int fd)
 {
 	size_t	len;
 
+	if (s == 0)
+		return ;
 	len = ft_strlen(s);
 	write(fd, s, (len + 1));
 }
