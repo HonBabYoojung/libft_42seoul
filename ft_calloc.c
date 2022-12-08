3d161feb5d687e62bf5ee8dce71a0f99026136a5
Sungoh Kim <sungohki@c6r1s8.42seoul.kr>
2022-12-08 19:09:34 +0900

    re calloc fix

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: sungohki <sungohki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 17:31:52 by sungohki          #+#    #+#             */
-/*   Updated: 2022/12/08 18:54:48 by sungohki         ###   ########.fr       */
+/*   Updated: 2022/12/08 19:09:22 by sungohki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	*ft_calloc(size_t count, size_t size)
 
 	index = 0;
 	temp = malloc(size * count);
-	if (temp == 0 || size >= (SIZE_MAX / count))
+	if (temp == 0)
 		return (0);
 	ft_bzero(temp, count * size);
 	return (temp);
