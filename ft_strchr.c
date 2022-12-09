1b87b55e26211f00ef40ecb48ba260dbcaf527fb
Sungoh Kim <sungohki@c6r2s8.42seoul.kr>
2022-12-09 23:44:36 +0900

    strchr strrchr fix

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: sungohki <sungohki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 16:48:47 by sungohki          #+#    #+#             */
-/*   Updated: 2022/12/09 22:03:35 by sungohki         ###   ########.fr       */
+/*   Updated: 2022/12/09 23:41:24 by sungohki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ char	*ft_strchr(const char *s, int c)
 {
 	char	*result;
 
-	if (s == 0)
-		return (0);
 	result = (char *)s;
 	while (*result)
 	{
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,30 +6,29 @@
 /*   By: sungohki <sungohki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 09:14:59 by sungohki          #+#    #+#             */
-/*   Updated: 2022/12/09 22:19:34 by sungohki         ###   ########.fr       */
+/*   Updated: 2022/12/09 23:41:32 by sungohki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 char	*ft_strrchr(const char *s, int c)
 {
 	char	*result;
+	char	convert_c;
 
-	if (s == 0)
-		return (0);
 	result = (char *)s;
+	convert_c = (char) c;
 	while (*result)
 		result++;
-	if ((unsigned char)c == 0)
-		return (result);
-	while (*s != *result)
+	while (s != result)
 	{
-		if ((unsigned char)*result == (unsigned char)c)
+		if (*result == convert_c)
 			return (result);
 		result--;
 	}
-	if ((unsigned char)*result == (unsigned char)c)
+	if (*result == convert_c)
 		return (result);
 	else
 		return (0);
