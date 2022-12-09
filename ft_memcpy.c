92f413bae3e69b767be47ad544f384887c032a3a
Sungoh Kim <sungohki@c6r2s8.42seoul.kr>
2022-12-10 00:23:55 +0900

    fix memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: sungohki <sungohki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 19:24:35 by sungohki          #+#    #+#             */
-/*   Updated: 2022/11/28 12:08:52 by sungohki         ###   ########.fr       */
+/*   Updated: 2022/12/10 00:23:31 by sungohki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,17 @@ void	*ft_memcpy(void	*dst, const void *src, size_t n)
 {
 	unsigned char			*dest;
 	unsigned const char		*source;
+	size_t					index;
 
 	dest = (unsigned char *)dst;
 	source = (unsigned const char *)src;
+	index = 0;
 	if (dst == src)
 		return (dst);
-	while (n--)
-		dest[n] = source[n];
+	while (index < n)
+	{
+		dest[index] = source[index];
+		index++;
+	}
 	return (dst);
 }
