b3726285a35ec58901752ea012fb80771ee6c0d9
Sungoh Kim <sungohki@c6r2s8.42seoul.kr>
2022-12-10 00:40:47 +0900

    strlcpy fix : add src_len

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: sungohki <sungohki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 10:10:50 by sungohki          #+#    #+#             */
-/*   Updated: 2022/11/28 12:49:55 by sungohki         ###   ########.fr       */
+/*   Updated: 2022/12/10 00:40:02 by sungohki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,15 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-	if (ft_strlen(src) + 1 < dstsize)
-		dstsize = ft_strlen(src) + 1;
+	size_t	src_len;
+
+	src_len = ft_strlen(src);
+	if (src_len + 1 < dstsize)
+		dstsize = src_len + 1;
 	if (dst == src || dstsize == 0)
-		return (ft_strlen(src));
+		return (src_len);
 	dst[--dstsize] = 0;
 	while (dstsize--)
 		dst[dstsize] = src[dstsize];
-	return (ft_strlen(src));
+	return (src_len);
 }
