1f00e858d31b2b35bc770ed35cc6c621ca9f3467
Sungoh Kim <sungohki@c2r1s4.42seoul.kr>
2022-12-09 21:48:51 +0900

    fix strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: sungohki <sungohki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 12:43:35 by sungohki          #+#    #+#             */
-/*   Updated: 2022/11/23 16:07:10 by sungohki         ###   ########.fr       */
+/*   Updated: 2022/12/09 21:48:16 by sungohki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,18 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	index = 0;
 	if (*needle == 0)
 		return ((char *)haystack);
+	if (len > ft_strlen(haystack))
+		len = ft_strlen(haystack) + 1;
 	while (haystack[index] && index < len)
 	{
 		if (haystack[index] == needle[0])
 		{
 			sub = 0;
 			while (needle[sub] && needle[sub] == haystack[index + sub]
-				&& index + sub < len)
+				&& haystack[index + sub] && index + sub < len)
 				sub++;
 			if (needle[sub] == 0)
 				return (&((char *)haystack)[index]);
-			index += sub;
 		}
 		index++;
 	}
